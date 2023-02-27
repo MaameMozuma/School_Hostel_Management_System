9ae1090740e8074834402a29dfa64e40877619b3
Adelle Hasford <111775657+Addy4H@users.noreply.github.com>
2023-02-27 22:31:28 +0000

    Admin side queries
    - made some edits to the room_avail file. However, changes need to be made to fix the query better. The first 4 are in working order
    - queries for cleaner schedule and students are also done and active

---
+++ SQL queries/booked_students.sql
@@ -0,0 +1,7 @@
+USE bayview_hostels;
+
+SELECT CONCAT(`first_name`, ' ', `last_name`) 'Name', `user_email` 'Email', `user_tel` 'Phone Number', `room_name` 'Room', `block_name` 'Hostel'
+FROM
+((booking INNER JOIN hostel_user ON booking.student_id = hostel_user.user_id) INNER JOIN room on booking.room_id = room.room_id) INNER JOIN block on room.block_id = block.block_id
+WHERE year_id IN (SELECT academic_year.year_id FROM academic_year WHERE a_year = "2022/2023")
+AND term_id IN (SELECT school_term.term_id FROM school_term WHERE term = "Term Two") AND booking.status = "Yes"
---
+++ SQL queries/cleaner_schedule_ish.sql
@@ -0,0 +1,4 @@
+USE bayview_hostels;
+
+SELECT `fname`, `lname`, `cleaning_area`, `no_of_rooms`, `tel_no`
+FROM `cleaner`
